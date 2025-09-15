2688f99f8349ea3c6ec6a370dffcc5355388c44e
Kunal Bachhao <kunalbachhao@gmail.com>
2025-09-15 23:07:11 +0530

    commit

--- models/user.js
+++ models/user.js
@@ -1,6 +1,3 @@
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://127.0.0.1:27017/DBMS`)
-
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
